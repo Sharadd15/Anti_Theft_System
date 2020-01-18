a96db90234b1381f46cf3bf71b98594d8699d51b
Sharad Shukla <sharadshukla429@gmail.com>
2020-01-18 22:17:09 +0000

    server code wotking for client2 and client3 simultaneously

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ bucket = storage.bucket('smart-anti-theft.appspot.com')
 
 def on_message(client, userdata, message):
     print "Message received: "  + message.payload
-    if message.payload == "INTRUDER ALERT!":
+    if message.payload == "INTRUDER ALERT FROM FRONT DOOR!":
         camera.start_preview()
         time.sleep(5)
         now = datetime.datetime.now()
@@ -29,13 +29,28 @@ def on_message(client, userdata, message):
 
         blob = bucket.blob(str(now) + ".jpg")
         blob.upload_from_filename(pic_path)
+    
+    if message.payload == "INTRUDER ALERT FROM WINDOW!":
+        camera.start_preview()
+        time.sleep(5)
+        now = datetime.datetime.now()
+        pic_path = "pict/" + str(now) + ".jpg"
+        camera.capture(pic_path)
+        camera.stop_preview()
+        db.collection(u'alerts').add({
+            u'timestamp': now,
+            u'location': u'Window'
+        })
 
+        blob = bucket.blob(str(now) + ".jpg")
+        blob.upload_from_filename(pic_path)
+    
 client.on_message= on_message   
 client.connect("pi-server.local", 1883, 60)
 print("connected")
 client.loop_start()
- 
 client.subscribe("client2")
+client.subscribe("client3")
  
 try:
     while True:
